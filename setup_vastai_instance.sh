c71ba53bf778bf907e90f65b5a1db202f6aac56d
antoninsumner <antonin.sumner@gmail.com>
2025-04-11 14:45:28 +0200

    Add Waymo environment setup and training scripts
    - Updated `environment.yml` to include `wandb` as a dependency.
    - Changed permissions of `extract_processed_waymo.sh` to make it executable.
    - Introduced `setup_vastai_instance.sh` for automating the setup of the Pointcept repository and Waymo dataset.
    - Added `train_ptv3_waymo.sh` script for training with Waymo data using GPU resources.
    - Created `setup_waymo_env.sh` to establish a conda environment and install necessary packages for Waymo dataset processing.

---
+++ setup_vastai_instance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /workspace
+
+echo "### CLONING POINTCEPT REPO"
+git clone https://github.com/Brainkite/Pointcept.git
+cd Pointcept
+
+echo "### DOWNLOAD WAYMO DATASET"
+bash ./download_waymo_HF.sh
+
+echo "### EXTRACT WAYMO DATASET"
+bash ./extract_waymo_dataset.sh
+
+echo "### INSTALL ENV"
+bash ./setup_pointcept_env.sh
+
+echo "### SETUP FINISHED"
