36c0b21f88568e3e75476964639a065871521fdc
antoninsumner <antonin.sumner@gmail.com>
2025-04-11 15:09:33 +0200

    Update `train_ptv3_waymo.sh` to simplify WandB login by removing the use of the WANDB_KEY environment variable.

--- train_ptv3_waymo.sh
+++ train_ptv3_waymo.sh
@@ -3,5 +3,5 @@ GPUS=$(( $(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l) ))
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pointcept
-wandb login $WANDB_KEY
+wandb login
 sh scripts/train.sh -g $GPUS -d waymo -c semseg-pt-v3m1-0-base -n semseg-pt-v3m1-0-base
